453dd4b787c8076725619ca638526db6552a7565
Md. Tariqul Islam <pranthaislam019@gmail.com>
2024-10-12 10:24:00 +0600

    Add config for deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,17 +13,18 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/webmaster/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'CodeCrazeTech', // Usually your GitHub org/user name.
   projectName: 'webmaster', // Usually your repo name.
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  deploymentBranch: 'main',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
